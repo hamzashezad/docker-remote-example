359ec4c74a1062f98b1c40d244d95023f322ef31
hamzashezad <9590983+hamzashezad@users.noreply.github.com>
2022-05-22 01:03:14 +0500

    testing

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get("/", (req, res) => {
 	res.send("Hello from Hell");
 });
 
+app.get("/version", (req, res) => {
+	res.send("Version 1.1.0");
+});
+
 app.listen(3000, () => {
 	console.log("Listening on port 3000");
 });
